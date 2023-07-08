60892b3ed7c7027cd94e5c1aa1d6561eb3c0b2e9
Cody17 <teror1716@yandex.ru>
2023-07-08 21:34:11 +0300

    Lesson 4.2 fixtures

---
+++ tests/the_fourth_lessons_test.py
@@ -0,0 +1,14 @@
+
+from configuration import SERVICE_URL
+from src.baseclasses.response import Response
+from src.schemas.user import User
+import requests
+
+
+res = requests.get(SERVICE_URL)
+
+
+def test_getting_users_list(say_hello):
+    response = requests.get(SERVICE_URL)
+    test_object = Response(response)
+    test_object.assert_status_code(200).validate(User)
