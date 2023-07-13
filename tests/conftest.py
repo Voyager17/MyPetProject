bd578f5a94a96bd7f61b26f4d537b27b52185e05
Cody17 <teror1716@yandex.ru>
2023-07-13 10:11:02 +0300

    changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -17,7 +17,10 @@ def get_number():
 
 
 def _calculate(a, b):
-    return a + b
+    if isinstance(a, int) and isinstance(b, int):
+        return a + b
+    else:
+        return None
 
 
 @pytest.fixture
--- tests/the_fourth_lessons_test.py
+++ tests/the_fourth_lessons_test.py
@@ -8,7 +8,7 @@ import requests
 res = requests.get(SERVICE_URL)
 
 
-def test_getting_users_list(say_hello):
+def test_getting_users_list():
     response = requests.get(SERVICE_URL)
     test_object = Response(response)
     test_object.assert_status_code(200).validate(User)
