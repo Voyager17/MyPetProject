152b4a8ca8011d467b2102ca391cc7fc6c84ad63
Cody17 <teror1716@yandex.ru>
2023-07-26 10:30:53 +0300

    lesson 7.2 generators

--- src/generators/player.py
+++ src/generators/player.py
@@ -1,11 +1,12 @@
 from src.enums.user_enums import Statuses
 from src.generators.player_localisation import PlayerLocalisation
+from src.baseclasses.builder import BuilderBaseClass
 
 
-class Player:
+class Player(BuilderBaseClass):
 
     def __init__(self) -> None:
-        self.result = {}
+        super().__init__()
         self.reset()
 
     def set_status(self, status=Statuses.active.value):
@@ -29,10 +30,3 @@ class Player:
             "ru": PlayerLocalisation('ru_RU').build()
         }
         return self
-
-    def update_inner_generator(self, key, generator):
-        self.result[key] = {"en": generator.build()}
-        return self
-
-    def build(self):
-        return self.result
--- tests/users/test_generator.py
+++ tests/users/test_generator.py
@@ -34,7 +34,10 @@ def test_deleting(delete_key, get_player_generator):
     print(object_to_send)
 
 
-def test_update_inner_generator(get_player_generator):
-    object_to_send = get_player_generator.update_inner_generator(
-        "localize", PlayerLocalisation("fr_FR").set_number(15)).build()
+@pytest.mark.parametrize("localizations, loc", [
+    ("fr", "fr_FR")
+])
+def test_update_inner_generator(get_player_generator, localizations, loc):
+    object_to_send = get_player_generator.update_inner_value(
+        ["localize", localizations], PlayerLocalisation(loc).set_number(15).build()).build()
     print(object_to_send)
